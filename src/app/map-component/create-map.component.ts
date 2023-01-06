8d0c6e5f3a6db3a01a6846ac620f8a48ca03ed44
Kavitha <kavithaselvaraj002@gmail.com>
2023-01-05 19:12:14 -0500

    add marker to map

--- src/app/map-component/create-map.component.ts
+++ src/app/map-component/create-map.component.ts
@@ -1,7 +1,7 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Loader} from '@googlemaps/js-api-loader'
+import { truncateSync } from 'fs';
 
 @Component({
   selector: 'app-create-map',
@@ -10,24 +10,42 @@ import { Loader} from '@googlemaps/js-api-loader'
 })
 export class CreateMapComponent implements OnInit {
   title = 'google-maps';
-  //map: google.maps.Map;
-
+  map: google.maps.Map;
+  //private map  = google.maps.Map;
 
   ngOnInit() {
     let loader = new Loader({
-      apiKey: 'AIzaSyC7rysP9Rbn5Z0FNbi-fG6Q_2xP7x2vfVA'
+      apiKey: 'API KEY'
 
     })
     
     loader.load().then(()=>{
-      const map =new google.maps.Map(document.getElementById("map"),{
-        center:{lat: 19.289193419745192,lng: -81.36917155103163},
+      const location = {
+        lat:19.289193419745192,
+        lng: -81.36917155103163
+      };
+      const location2 = {
+        lat:19.29810454482719,
+        lng: -81.35758441835928
+      };
+     // 19.29810454482719, -81.35758441835928
+       this.map =new google.maps.Map(document.getElementById("map"),{
+        center:location,
         zoom: 15
       })
+
+      const marker = new google.maps.Marker({
+        position:location2,
+        map:this.map,
+        title:"test",
+       // icon: '../assets/icons/caricon.png'
+      })
     })
+   
     
   }
 
   
 
 }
+
